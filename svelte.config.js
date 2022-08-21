0385cd1407d55fdcdf3f9d65fbf1c860c960db49
Piers Rollinson <piers.rollinson@gmail.com>
2022-08-20 17:38:58 -0700

    Remove prerendering

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 import { mdsvex } from 'mdsvex';
 
@@ -21,7 +21,7 @@ const config = {
 	kit: {
 		adapter: adapter(),
 		prerender: {
-      default: true
+      default: false
     },
 	},
 };
